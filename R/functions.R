173c1557478a51eabd8683c07c561273ab574f09
RAI <arai2@monsanto.com>
2020-09-24 16:27:50 +0530

    changed the result

--- R/functions.R
+++ R/functions.R
@@ -1,9 +1,17 @@
+#' Title
+#'
+#' @param number
+#'
+#' @return even or odd
+#' @export
+#'
+#' @examples
 even_odd <- function(number) {
   remainder <- number %% 2
   if (remainder == 0) {
-    result <- "Even"
+    result <- "Number is Even"
   } else {
-    result <- "Odd"
+    result <- "Number is Odd"
   }
   return(result)
 }
